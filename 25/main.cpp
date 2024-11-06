570e5128eb34d862481bbb56f1478052d29a4312
cooper <zhangzhao0608@gmail.com>
2024-11-06 16:57:01 +0800

    ds

---
+++ 25/main.cpp
@@ -0,0 +1,52 @@
+//25. K 个一组翻转链表
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+
+
+
+        
+
+        while (true) {
+            //找到k个中的头和尾
+            // 找第k个的下一个        
+        }
+
+        // 进行翻转 
+        while (true) {
+        
+        }
+
+        //递归调用
+
+
+
+
+
+
+
+
+
+        //返回新的node
+        return nullptr;
+        
+    }
+};
+
+int main() {
+
+    return 0;
+
+}
+
+
+
