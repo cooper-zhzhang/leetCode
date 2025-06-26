b80c1011d27fb9a24cc2046a27e4c49fa8c20e93
cooper-zhzhang <zhangzhao0608@gmail.com>
2025-06-26 16:10:18 +0800

    树状数组的解法

--- 315/main.cpp
+++ 315/main.cpp
@@ -39,9 +39,85 @@ https://leetcode.cn/problems/count-of-smaller-numbers-after-self
 *
 */
 
+#include <cstring>
 #include <vector>
 #include <iostream>
+#include <set>
+#include <algorithm>
+
+
 using namespace std;
+
+
+
+class FenwickArray {
+    public:
+    FenwickArray(int maxSize):MAXN(maxSize), bit(maxSize, 0){
+
+    }
+
+    void add(int i, int x) {
+        while (i < MAXN) {
+            bit[i] += x;
+            i += lowbit(i);
+        }
+    }
+
+    void sub(int i, int x) {
+        while (i < MAXN) {
+            bit[i] -= x;
+            i += lowbit(i);
+        }
+    }
+
+    int sum(int i) {
+        int s = 0;
+        while (i > 0) {
+            s += bit[i];
+            i -= lowbit(i);
+        }
+        return s;
+    }
+    private:
+    vector<int>bit;
+    int MAXN;
+
+    int lowbit(int x) {
+        return x&(-x);
+    }
+};
+
+
+class Solution2 {
+public:
+    static const int MAXN = 10;
+    int x[MAXN], y[MAXN];
+
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            x[i] = y[i] = nums[i];
+        }
+
+        FenwickArray fenArray(MAXN);
+
+        sort(y, y + n);
+        int len = unique(y, y + n) - y;
+        for (int i = 0; i < n; ++i) {
+            x[i] = lower_bound(y, y + len, x[i]) - y + 1;
+        }
+
+        vector<int> res;
+        for (int i = n-1; i >= 0; --i) {
+            res.push_back(fenArray.sum(x[i]-1));
+            fenArray.add(x[i], 1);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+
 
 
 class Solution {
@@ -69,7 +145,8 @@ public:
     vector<int> countSmaller(vector<int>& nums) {
 
 		vector<int> ret(nums.size(), 0);
-		vector<int>sortedNums ;
+		vector<int>sortedNums;
+		sortedNums.reserve(nums.size());
 		for (int i = nums.size()-1; i >= 0; --i){
 			int index = insert(sortedNums, nums[i]);
 			ret[i] = index;
@@ -81,8 +158,9 @@ public:
 
 int main() {
 
-	vector<int> nums = {-1, -1};
-	auto ret = Solution().countSmaller(nums);
+	//vector<int> nums = {26,78,27,100,33,67,90,23,66,5,38,7,35,23,52,22,83,51,98,69,81,32,78,28,94,13,2,97,3,76,99,51,9,21,84,66,65,36,100,41};
+	vector<int> nums ={5,2,6,1};
+	auto ret = Solution2().countSmaller(nums);
 	for (auto &v : ret){
 		std::cout << v << " ";
 	}
@@ -91,3 +169,20 @@ int main() {
 
 	return 0;
 }
+
+
+
+
+/*
+lowbit(12) = 4
+lowbit(11) = 1
+lowbit(10) = 2
+lowbit(9) = 1
+lowbit(8) = 8
+lowbit(7) = 1
+lowbit(6) = 2
+lowbit(5) = 1
+lowbit(4) = 4
+lowbit(3) = 1
+lowbit(2) = 2
+lowbit(1) = 1*/
